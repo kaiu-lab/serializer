b233c07fde5bd1d7987cbf57ef22d03904858cc6
supamiu <contact@flavien-normand.fr>
2017-08-01 09:22:31 +0200

    fix: added FieldName to exported members.

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -1,3 +1,4 @@
 export { DeserializeAs } from './deserialize-as';
 export { Parent } from './parent';
 export { DeserializeFieldName } from './deserialize-field-name';
+export { FieldName } from './field-name';
