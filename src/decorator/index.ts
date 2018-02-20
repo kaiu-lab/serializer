4e95f0cc59ad7ca37e5d858910ca1f4f689a9b9e
supamiu <contact@flavien-normand.fr>
2018-02-20 11:30:40 +0100

    fix: add new decorators to index file

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -1,4 +1,6 @@
 export { DeserializeAs } from './deserialize-as';
 export { Parent } from './parent';
 export { DeserializeFieldName } from './deserialize-field-name';
+export { SerializeFieldName } from './serialize-field-name';
 export { FieldName } from './field-name';
+export { Transient } from './transient';
